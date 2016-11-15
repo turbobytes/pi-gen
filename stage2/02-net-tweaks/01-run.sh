a1d3c944ff337f2058b60edda742028e90f8ccab
Sajal Kayan <sajal@turbobytes.com>
2016-11-15 13:18:11 +0700

    cut some packages

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,6 +3,6 @@
 install -v -d						${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d
 install -v -m 644 files/wait.conf			${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/
 
-install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
-install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
+#install -v -d                                           ${ROOTFS_DIR}/etc/wpa_supplicant
+#install -v -m 600 files/wpa_supplicant.conf             ${ROOTFS_DIR}/etc/wpa_supplicant/
 
